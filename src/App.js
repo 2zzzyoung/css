6903d1b2b91cc707972a9fc62a86136b404f9330
2zzzyoung <mzooyoungn@gmail.com>
2023-03-06 23:49:32 +0900

    postCSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import "./App.css";
+import Button1 from "./components/Button1";
+import Button2 from "./components/Button2";
 
 function App() {
   return (
-   
+    <>
+      <Button1 />
+      <Button2 />
+    </>
   );
 }
 
