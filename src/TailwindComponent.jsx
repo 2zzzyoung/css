59d8c3846e0f0a266f9ee762c0662795d0065dd7
2zzzyoung <mzooyoungn@gmail.com>
2023-03-07 22:01:42 +0900

    tailwindComponent

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TailwindComponent() {
+  return (
+    <div>
+      <h1 className="text-5xl">Hi!</h1>
+      <button className="bg-blue-400 rounded-xl px-2">Button</button>
+    </div>
+  );
+}
